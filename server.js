e774debb78631ef1b9ba9fdc7863574969cdb195
m9672966-boop <m9672966@gmail.com>
2025-08-22 13:19:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,17 @@ const app = express();
 // Разрешаем все CORS-запросы
 app.use(cors({ origin: '*' }));
 
-// Статические файлы
-app.use(express.static(path.join(__dirname, 'public')));
+// Отдаём файлы из корня проекта
+app.use(express.static(path.join(__dirname)));
+
+// Обязательно добавим маршрут на корень, чтобы index.html открывался по умолчанию
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/quotes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'quotes.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 
